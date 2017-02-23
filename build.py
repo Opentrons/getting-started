4413c2e30bdd02dbd898337a13fbfd327d42bfe5
andySigler <andrewsigler1@gmail.com>
2017-02-23 10:31:43 -0500

    adds script for building html renderings from notebooks; adds custom styling

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os
+import shutil
 
 
+os.system("find . -name '*.html' | xargs rm -rf")
 os.system("find . -name '.ipynb_checkpoints' | xargs rm -rf")
 os.system(
     "find . -name '*.ipynb' | xargs jupyter nbconvert --to html --execute --allow-errors")
@@ -10,8 +12,18 @@ os.system("find . -name 'smoothie' | xargs rm -rf")
 os.system("find . -name 'logs' | xargs rm -rf")
 os.system("find . -name 'logs' | xargs rm -rf")
 
+
 # move files to separate folder
+def get_files(path):
+    for d in os.listdir(path):
+        filepath = os.path.join(path, d)
+        if os.path.isdir(d) and '.' not in d:
+            get_files(filepath)
+        elif '.html' in d:
+            try:
+                shutil.move(filepath, './html')
+            except Exception as e:
+                print(e)
 
-# parse .html files and modify CSS for Cells' stdout
 
-print('\n\ndone!!')
+get_files('.')
